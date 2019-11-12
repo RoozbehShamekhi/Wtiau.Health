1b95b6d58a62dd5b59cad81b7c3587da0ec79916
roozbeh shamekhi <roozbehshamekhi@hotmail.com>
2019-11-12 21:03:20 +0330

    new commit

---
+++ Wtiau.Health.Web/Controllers/TurnController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wtiau.Health.Web.Controllers
+{
+    public class TurnController : Controller
+    {
+        // GET: Turn
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult TurnAdd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult TurnAdd(int id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult TurnEdit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult TurnEdit(int id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult TurnDelete()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult TurnDelete(int id)
+        {
+            return View();
+        }
+    }
+}
